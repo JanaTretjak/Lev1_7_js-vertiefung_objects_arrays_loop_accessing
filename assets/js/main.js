856803b1bb42e0e6e1858f8abdbe25c749cb1f99
JanaTretjak <tretiachechka@gmail.com>
2020-03-16 15:48:06 +0100

    ver 0

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+var myMusic = [
+    {
+        kunstler: "The Beatles",
+        title: "Abbey Road",
+        release_jahr: 1969,
+        formate: ["LP", "CD", "MC", "Download"],
+        gold: true
+    },
+    {
+        kunstler: "Pink Floyd",
+        title: "Dark Side of the Moon",
+        release_jahr: 1978,
+        formate: ["CS", "CD", "LP", "Download"],
+        gold: true
+    },
+    {
+        kunstler: "Led Zeppelin",
+        title: "Led Zeppelin IV",
+        release_jahr: 1971,
+        formate: ["CS", "LP", "Download"],
+        gold: true
+    },
+    {
+        kunstler: "Metallica",
+        title: "Kill ’Em All und Ride the Lightning",
+        release_jahr: 1983,
+        formate: ["LP", "CD", "MC", "Download"],
+        gold: true
+    }
+];
+
+myMusic.forEach(element => console.log(element.kunstler));
+myMusic.forEach(element => console.log(element.title));
+myMusic.forEach(element => console.log(element.formate));
+myMusic.forEach(element => {
+    if (element.release_jahr > 1975) {
+        console.log(element.release_jahr)
+    }
+});
